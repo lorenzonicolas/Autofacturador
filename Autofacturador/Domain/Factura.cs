6e3654689e9476d7bee3ee618c90efb754f6f82c
Nicolas Lorenzo <lorenzo_nicolas@hotmail.com.ar>
2024-04-10 11:25:09 -0300

    First commit

---
+++ Autofacturador/Domain/Factura.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace BrowserAutomation
+{
+    public class Factura
+    {
+        public string Codigo { get; set; }
+        public int CantidadHoras { get; set; }
+        public int CantidadItems { get; set; }
+        public int PrecioPorHora { get; set; }
+        public string Descripcion { get; set; }
+        public TipoDeFactura Tipo { get; set; }
+
+        public Factura(TipoDeFactura tipoFactura)
+        {
+            this.PrecioPorHora = int.Parse(TestContext.Parameters[$"{tipoFactura}_precio_hora"]);
+            this.CantidadHoras = int.Parse(TestContext.Parameters[$"{tipoFactura}_cantidad_horas"]);
+            this.Descripcion = TestContext.Parameters[$"{tipoFactura}_descripcion"];
+            this.Codigo = TestContext.Parameters[$"{tipoFactura}_codigo"];
+            this.CantidadItems = int.Parse(TestContext.Parameters[$"{tipoFactura}_cantidad_items"]);
+            this.Tipo = tipoFactura;
+        }
+    }
+}
